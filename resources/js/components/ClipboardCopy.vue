b408f689ce37a390ce31fa349b5c511b6a6866e4
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-03-14 08:07:37 +0000

    Added ability to copy a shortened URL to clipboard

---
+++ resources/js/components/ClipboardCopy.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Button } from '@/components/ui/button';
+import Icon from '@/components/Icon.vue';
+
+const props = withDefaults(defineProps<{
+    value: string,
+    iconOnly?: boolean,
+}>(), {
+    iconOnly: false,
+});
+
+const emit = defineEmits<{
+    'copied': [value: string],
+}>();
+
+const canShow: boolean = window.location.protocol === 'https:';
+
+if (! canShow) {
+    console.warn('Copy to clipboard will only work on https://');
+}
+
+const copied = ref(false)
+
+const copy = () => {
+    if (! canShow || ! props.value) {
+        return;
+    }
+
+    window.navigator.clipboard.writeText(props.value).then(() => {
+        emit('copied', props.value);
+
+        copied.value = true
+        setTimeout(() => copied.value = false, 3000);
+    });
+}
+</script>
+
+<template>
+    <Button variant="link" v-if="canShow" @click="copy" class="gap-1 hover:no-underline">
+        <Icon v-if="copied" name="Check" />
+        <Icon v-else name="ClipboardCopy" />
+
+        <span
+            v-show="!iconOnly"
+            v-text="copied ? 'Copied' : 'Copy'"
+            :class="{ 'sr-only': iconOnly }"
+        />
+    </Button>
+</template>
--- resources/js/components/ShortenedUrlResults.vue
+++ resources/js/components/ShortenedUrlResults.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-defineProps<{
+import ClipboardCopy from '@/components/ClipboardCopy.vue';
+
+interface Props {
     url?: {
         shortened: string,
         original: string,
         visits: number,
-        lastVisitedAt?: string
-    }
-}>();
+        lastVisitedAt?: string,
+    },
+};
+
+defineProps<Props>();
 </script>
 
 <template>
     <div v-if="url" class="mt-10 flex-1 rounded-lg bg-zinc-50 p-6 py-8 leading-tight shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
-        <div class="flex justify-center flex-row text-center">
+        <div class="flex justify-center items-center flex-col text-center">
             <p class="text-2xl mr-2">{{ url.shortened }}</p>
+            <ClipboardCopy :value="url.shortened" />
         </div>
 
         <div class="lg:w-1/2 mx-auto pt-6 mt-6 border-t-2 space-y-3">
