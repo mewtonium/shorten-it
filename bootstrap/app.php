08dce7240112d505abd7178e385d93448e9425bb
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-04-06 14:14:26 +0100

    Added expired session/CSRF handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Illuminate\Http\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -28,5 +29,16 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->respond(function (Response $response) {
+            if ($response->getStatusCode() === 419) {
+                return back()->with([
+                    'banner' => [
+                        'type' => 'danger',
+                        'message' => 'The page expired, please try again',
+                    ],
+                ]);
+            }
+
+            return $response;
+        });
     })->create();
