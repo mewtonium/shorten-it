9b9a0c7af58e9c0ae79f432fa295988c7a1aed03
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-04-06 14:22:38 +0100

    Fixed incorrect route name

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'Home')->name('home');
 
 Route::middleware('auth.enabled')->group(function () {
-    Route::inertia('/dashboard', 'Dashboard')->middleware(['auth', 'verified'])->name('home');
+    Route::inertia('/dashboard', 'Dashboard')->middleware(['auth', 'verified'])->name('dashboard');
 
     require __DIR__.'/settings.php';
     require __DIR__.'/auth.php';
