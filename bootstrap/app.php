3b7a4c89519e2955c3bd67c81c22c2b298e36604
Mark Wilkinson <56736714+mewtonium@users.noreply.github.com>
2025-04-12 22:07:41 +0100

    Fixed failing tests when auth is enabled

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
